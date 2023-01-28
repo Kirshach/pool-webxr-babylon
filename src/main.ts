b8bf9a75cc089bb5e8a0348623eb5b05d36872e4
kirshach <kirshach@mail.ru>
2023-01-28 17:17:46 +0400

    feat: TS 4.9 -> 5.0, add bg music

--- src/main.ts
+++ src/main.ts
@@ -6,34 +6,37 @@ import {
   HemisphericLight,
   MeshBuilder,
   Scene,
-  Vector3
-} from '@babylonjs/core';
+  Sound,
+  Vector3,
+} from "@babylonjs/core";
 
-import '@babylonjs/loaders'; // TODO: 667kB, need to tree-shake it
+import "@babylonjs/loaders"; // TODO: 667kB, need to tree-shake it
 
-import { getPoolTable } from './pool-table';
+import { getPoolTable } from "./pool-table";
 
 DracoCompression.Configuration = {
   decoder: {
-    wasmUrl: '/draco/draco_wasm_wrapper_gltf.js',
-    wasmBinaryUrl: '/draco/draco_decoder_gltf.wasm',
-    fallbackUrl: '/draco/draco_decoder_gltf.wasm',
+    wasmUrl: "/draco/draco_wasm_wrapper_gltf.js",
+    wasmBinaryUrl: "/draco/draco_decoder_gltf.wasm",
+    fallbackUrl: "/draco/draco_decoder_gltf.wasm",
   },
 };
 
-const canvas = document.getElementById('canvas');
+const canvas = document.getElementById("canvas");
 
 if (!canvas) {
-  throw new Error('Canvas not found');
+  throw new Error("Canvas not found");
 } else if (!(canvas instanceof HTMLCanvasElement)) {
-  throw new Error('"#canvas" element was found but is not instance of HTMLCanvasElement');
+  throw new Error(
+    '"#canvas" element was found but is not instance of HTMLCanvasElement'
+  );
 }
 
 const engine = new Engine(canvas, true, {});
 
 const scene = new Scene(engine);
 const camera = new ArcRotateCamera(
-  'camera',
+  "camera",
   Math.PI / 2 - Math.PI / 4,
   Math.PI * 0.4,
   5,
@@ -42,16 +45,27 @@ const camera = new ArcRotateCamera(
 );
 camera.attachControl(canvas, false);
 
-new HemisphericLight('light', new Vector3(0.1, 0.1, 0), scene);
-MeshBuilder.CreateGround('ground', { width: 10, height: 10 });
-
+// add lights
+new HemisphericLight("light", new Vector3(0.1, 0.1, 0), scene);
+// add ground
+MeshBuilder.CreateGround("ground", { width: 10, height: 10 });
+// add table
 await getPoolTable(scene);
 
+// add background music
+new Sound(
+  "Mark Woollard - My Heart Is Home",
+  "/assets/music/melodyloops-preview-my-heart-is-home-1m27s.mp3",
+  scene,
+  null,
+  { loop: true, autoplay: true }
+);
+
 // TODO: move to a dev-time button
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   new AxesViewer(scene, 0.35);
-  import('@babylonjs/inspector').then(() => {
-    Object.defineProperty(window, 'debug', {
+  import("@babylonjs/inspector").then(() => {
+    Object.defineProperty(window, "debug", {
       get() {
         return this._debug;
       },
@@ -62,11 +76,11 @@ if (process.env.NODE_ENV === 'development') {
         } else {
           scene.debugLayer.hide();
         }
-      }
+      },
     });
   });
 }
 
-
+// run the loop
 engine.runRenderLoop(() => scene.render());
-window.addEventListener('resize', () => engine.resize());
+window.addEventListener("resize", () => engine.resize());
