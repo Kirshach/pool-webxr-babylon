e683308efada14830724dad9455e83f8591db75b
kirshach <kirshach@mail.ru>
2023-02-13 03:27:32 +0400

    feat: use jpg for ground textures

--- src/entities/ground.ts
+++ src/entities/ground.ts
@@ -12,12 +12,12 @@ export const createGround = async (scene: Scene) => {
   const groundMaterial = new PBRMaterial('ground_material', scene);
 
   [
-    groundMaterial.albedoTexture = new Texture('/assets/ground-textures/diff.png', scene),
-    groundMaterial.metallicTexture = new Texture('/assets/ground-textures/arm.png', scene),
-    groundMaterial.bumpTexture = new Texture('/assets/ground-textures/normal.png', scene),
+    groundMaterial.albedoTexture = new Texture('/assets/ground-textures/diff.jpg', scene),
+    groundMaterial.metallicTexture = new Texture('/assets/ground-textures/arm.jpg', scene),
+    groundMaterial.bumpTexture = new Texture('/assets/ground-textures/normal.jpg', scene),
   ].forEach((texture) => {
-    texture.uScale = 4;
-    texture.vScale = 4;
+    texture.uScale = 8;
+    texture.vScale = 8;
   });
 
   groundMaterial.useAmbientOcclusionFromMetallicTextureRed = true;
