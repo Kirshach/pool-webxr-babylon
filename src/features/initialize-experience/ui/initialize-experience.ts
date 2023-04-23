0cd0add417ed1fc3fc0aa86aa536df5d5d5e85c8
kirshach <kirshach@mail.ru>
2023-04-23 20:39:22 +0400

    fix: thumbstick movement

--- src/features/initialize-experience/ui/initialize-experience.ts
+++ src/features/initialize-experience/ui/initialize-experience.ts
@@ -3,11 +3,13 @@ import {
   DracoCompression,
   Engine,
   HavokPlugin,
-  Quaternion,
+  Axis,
   Scene,
   ShadowGenerator,
   Sound,
   Vector3,
+  PhysicsRaycastResult,
+  Ray,
   // WebGPUEngine,
   WebXRDefaultExperience,
 } from "@babylonjs/core";
@@ -63,7 +65,7 @@ export const initializeExperience = async (canvas: HTMLCanvasElement) => {
     createGround(scene),
   ]);
 
-  const balls = Array.from({ length: 100 }).map(() => createBall(scene));
+  const balls = Array.from({ length: 20 }).map(() => createBall(scene));
 
   // shadows
   const shadowGenerator = new ShadowGenerator(2048, spotLight, true);
@@ -83,25 +85,96 @@ export const initializeExperience = async (canvas: HTMLCanvasElement) => {
     disableTeleportation: true,
   });
 
-  // Enable smooth movement with the VR controllers
+  // WebXR events
   xrExperience.input.onControllerAddedObservable.add((controller) => {
-    controller.onMotionControllerInitObservable.add((motionController) => {
-      if (motionController.handedness !== "left") return;
+    const yAxis = Axis.Y;
+    let currentAxes = { x: 0, y: 0 };
+    let targetMovementVector = new Vector3(0, 0, 0);
+    const lerpFactor = 0.1;
 
+    controller.onMotionControllerInitObservable.add((motionController) => {
+      // Enable smooth movement with the VR controllers
       const thumbstick = motionController.getComponent(
         "xr-standard-thumbstick"
       );
 
-      if (thumbstick) {
-        thumbstick.onAxisValueChangedObservable.add((axes) => {
-          // TODO: wtf is that and why do I have to rotate it?
-          const vec = new Vector3(-axes.y, 0, -axes.x).scale(0.05);
-          const quat = Quaternion.FromEulerAngles(0, 90, 0);
-          let vec2 = Vector3.Zero();
-          vec.rotateByQuaternionToRef(quat, vec2);
-          xrExperience.baseExperience.camera.position.addInPlace(vec2);
-        });
-      }
+      thumbstick.onAxisValueChangedObservable.add((axes) => {
+        currentAxes = axes;
+      });
+
+      let lastUpdateTime = performance.now();
+
+      scene.registerBeforeRender(() => {
+        const currentTime = performance.now();
+        const deltaTime = (currentTime - lastUpdateTime) / 1000; // Calculate delta time in seconds
+        lastUpdateTime = currentTime;
+
+        // Get the XR camera from the scene
+        var xrCamera = xrExperience.baseExperience.camera;
+
+        // Get the direction vector of the camera
+        var cameraDirection = xrCamera.getDirection(new Vector3(0, 0, 1));
+        cameraDirection.y = 0;
+
+        // Rotate the camera direction vector by 90 degrees around the up vector to get the right vector
+        var rightVector = Vector3.Cross(yAxis, cameraDirection).normalize();
+
+        // Combine the camera direction and right vectors to get the movement vector
+        var rawMovementVector = cameraDirection
+          .scale(-currentAxes.y)
+          .add(rightVector.scale(currentAxes.x))
+          .normalize();
+
+        // LERP between the current target movement vector and the raw movement vector
+        targetMovementVector = Vector3.Lerp(
+          targetMovementVector,
+          rawMovementVector,
+          lerpFactor
+        );
+
+        // Scale the movement vector by the desired speed, delta time factor and add it to the camera position
+        xrCamera.position.addInPlace(targetMovementVector.scale(deltaTime));
+      });
+
+      // push the ball
+      const triggerComponent = motionController.getComponent(
+        "xr-standard-trigger"
+      );
+
+      // Add an event listener for the trigger button
+      triggerComponent.onButtonStateChangedObservable.add((component) => {
+        if (component.pressed) {
+          // Create a Ray object to store the direction
+          const directionRay = new Ray(Vector3.Zero(), Vector3.Zero());
+
+          // Use the 'getWorldPointerRayToRef' method to update the Ray object with the current direction
+          controller.getWorldPointerRayToRef(directionRay);
+
+          // The 'direction' property of the Ray object contains the controller's direction
+          const controllerDirection = directionRay.direction;
+
+          // Cast a ray from the controller's position in the forward direction
+          const startPosition = directionRay.origin;
+          const endPosition = startPosition.add(controllerDirection.scale(100));
+          const raycastResult = new PhysicsRaycastResult();
+
+          // Perform the raycast
+          scene
+            .getPhysicsEngine()!
+            // @ts-ignore
+            .raycastToRef(startPosition, endPosition, raycastResult);
+
+          // Apply a force to the hit body if there's a hit
+          if (raycastResult.hasHit) {
+            const forceMagnitude = 1;
+            const force = controllerDirection.scale(forceMagnitude);
+            // @ts-ignore
+            raycastResult.body.mass = 1; // Ensure the hit body has mass before applying impulse
+            // @ts-ignore
+            raycastResult.body.applyImpulse(force, raycastResult.hitPointWorld);
+          }
+        }
+      });
     });
   });
 
